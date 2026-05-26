eee23557d15d9a9dc72a97957605a9457919c5b5
EchoDavidHinojosa <davidhinojosaecho@gmail.com>
2026-05-26 10:06:31 +0200

    Primer commit

---
+++ Practica3/App_Code/GestorBibliotecario_inf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GestorBibliotecario
+{
+    interface GestorBibliotecario_inf
+    {
+        int Conexion(String Passwd);
+        bool Desconexion(int Ida);
+        int AbrirRepositorio(int Ida, String NomFichero);
+        int GuardarRepositorio(int Ida, int Repo);
+        int NRepositorios(int Ida);
+        TDatosRepositorio DatosRepositorio(int Ida, int Repo);
+        int NuevoLibro(int Ida, TLibro Libro, int Repo);
+        int Comprar(int Ida, String Isbn, int NoLibros);
+        int Retirar(int Ida, String Isbn, int NoLibros);
+        bool Ordenar(int Ida, int Campo);    
+        int NLibros(int Repo);
+        int Buscar(int Ida, String Isbn);
+        TLibro Descargar(int Ida, int Repo, int Pos);
+        int Prestar(int Pos);
+        int Devolver(int Pos);
+    }
+}
